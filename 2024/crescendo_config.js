cdc6c9634941ba4b678f4ba7b046b0f65c62c9cf
GurmanPlayz <68207858+GurmanPlayz@users.noreply.github.com>
2024-02-10 16:17:59 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mifor",
+      "defaultValue": "2023mitor",
       "required": "true"
     },
     { "name": "Match Level",
