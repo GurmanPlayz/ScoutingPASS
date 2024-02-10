4c75d3d40cf913c145f655f005bb07192bf3b11e
GurmanPlayz <68207858+GurmanPlayz@users.noreply.github.com>
2024-02-10 16:28:46 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -8,6 +8,7 @@ var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6"
  * @param {eventCode} eventCode the event code (i.e. 2020caln) to pull the team list
  */
 function getTeams(eventCode) {
+	location.reload();
 	if (authKey) {
 		var xmlhttp = new XMLHttpRequest();
 		var url = "https://www.thebluealliance.com/api/v3/event/" + eventCode + "/teams/simple";
@@ -30,6 +31,7 @@ function getTeams(eventCode) {
  * @param {eventCode} eventCode the event code (i.e. 2020caln) to pull the team list
  */
 function getSchedule(eventCode) {
+	location.reload();
 	if (authKey) {
 		var xmlhttp = new XMLHttpRequest();
 		var url = "https://www.thebluealliance.com/api/v3/event/" + eventCode + "/matches/simple";
